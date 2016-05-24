ccf8c6508c3ab9d0a81efe71ab2e88a41d831160
Tobias Burgherr <tburgherr@gmail.com>
2016-05-24 10:20:31 +0200

    Update user list
    It now works with virtual-repeat.
    See: https://material.angularjs.org/latest/demo/virtualRepeat

--- main/public/modules/users/users-list/users.client.controller.js
+++ main/public/modules/users/users-list/users.client.controller.js
@@ -2,15 +2,35 @@
     'use strict';
     var module = angular.module('admin');
 
-    module.controller('UsersController', function($scope, Restangular) {
+    module.controller('UsersController', function($scope, Restangular,$log, $window,$timeout) {
+        $log.debug("[UserController] init")
 
-        var ctrl = this;
+        var self = this;
         var nextCursor = '';
         var more = true;
-        $scope.users = [];
 
-        ctrl.getUsers = function() {
-            if (!more) {
+        // flexible height fix for the virtual container
+        // https://github.com/angular/material/issues/4314
+        // might not be necessary in future angular material design versions
+        // TODO create a directive
+        $scope.listStyle = {
+            height: ($window.innerHeight - 240) + 'px'
+        };
+        $window.addEventListener('resize', onResize);
+        function onResize() {
+            $scope.listStyle.height = ($window.innerHeight - 240) + 'px';
+            $timeout($scope.$broadcast('$md-resize'),100);
+            if(!$scope.$root.$$phase){
+                //$scope.$digest();
+            }
+        }
+
+
+        $scope.users = [];
+        $scope.totalCount = 0;
+
+        self.getUsers = function() {
+            if (!more || $scope.isLoading) {
                 return;
             }
             $scope.isLoading = true;
@@ -24,12 +44,38 @@
             });
         };
 
-        ctrl.getUsers();
+        self.getUsers();
 
         // This is fired when user scrolled to bottom
         $scope.$on('mainContentScrolled', function() {
-            ctrl.getUsers();
+            //self.getUsers();
         });
+
+
+        // In this example, we set up our model using a plain object.
+        // Using a class works too. All that matters is that we implement
+        // getItemAtIndex and getLength.
+        $scope.repeatedUsers = {
+          // Required.
+          getItemAtIndex: function(index) {
+            var user = $scope.users[index]
+            if (user !== undefined) {
+              return user
+            }
+            if (more && !$scope.isLoading) {
+                self.getUsers();
+            }
+            return null;
+          },
+          // Required.
+          // For infinite scroll behavior, we always return a slightly higher
+          // number than the previously loaded items.
+          getLength: function() {
+            return $scope.totalCount;
+          }
+        };
+
+
 
     });
 }());

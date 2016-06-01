3d6d733b293a7ab05ef5a49c6711d0f991a92518
Tobias Burgherr <tburgherr@gmail.com>
2016-06-01 14:24:02 +0200

    Icon fix for auth accounts

--- main/public/modules/users/directives/gaAuthOptions.client.dir.js
+++ main/public/modules/users/directives/gaAuthOptions.client.dir.js
@@ -11,16 +11,28 @@
      * Also inserts 'remember me' checkbox, which can be binded by remember="myModel"
      */
 
-    module.directive('gaAuthOptions', function(gaAppConfig) {
+    module.directive('gaAuthOptions', function(gaAppConfig,$log) {
         var link = function(scope) {
-            scope.authOptions = _.keys(_.pick(gaAppConfig, function(cfg, cfgName) {
-                return _.startsWith(cfgName, 'auth_') && cfg;
+            $log.debug('[gaAuthOptions:link] init');
+            scope.authOptions = _.keys(_.pickBy(gaAppConfig, function(cfg, cfgName) {
+                return _.startsWith(cfgName, 'auth_') && _.endsWith(cfgName, '_id')&& cfg;
             }));
             scope.authOptions = _.map(scope.authOptions, function(optName) {
                 return optName.replace('auth_', '').replace('_id', '');
             });
             scope.authOptions.unshift('google');
             scope.remember = true;
+            
+            // Icons
+            var icons = _.pickBy(gaAppConfig, function(cfg, cfgName) {
+                $log.debug(cfg);
+                $log.debug(cfgName);
+                return _.startsWith(cfgName, 'auth_') && _.endsWith(cfgName, '_icon');
+            });
+            scope.authIcons = _.mapKeys(icons, function(value, key) {
+                return key.replace('auth_', '').replace('_icon', '');
+            });
+            scope.authIcons.google = "google"; // TODO at it to the server side config
         };
 
         return {
